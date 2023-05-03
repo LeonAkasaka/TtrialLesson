1cde3821457e109526746e3f9247abfb28b10305
Leon Akasaka <leon@wisdomsoft.jp>
2023-05-03 19:34:21 +0900

    Animator の取得は事前にやっておく。

--- Assets/Game/Player.cs
+++ Assets/Game/Player.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Animator _animator;
+
     private void Start()
     {
-
+        _animator = GetComponent<Animator>();
     }
 
     private void Update()
