476cda689693a54097cfc86f71cbf43005c84f1f
Leon Akasaka <leon@wisdomsoft.jp>
2023-07-13 01:43:44 +0900

    Player スクリプトのコードを最小化。
    インベーダーゲームとかで使い回す想定。

--- Assets/Game/Player.cs
+++ Assets/Game/Player.cs
@@ -1,13 +1,9 @@
 using UnityEngine;
-using Sample.MessageWindow;
 
 public class Player : MonoBehaviour
 {
-    private Animator _animator;
-
     private void Start()
     {
-        _animator = GetComponent<Animator>();
     }
 
     private void Update()
